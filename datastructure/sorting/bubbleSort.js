99a80eb4806ea5a8b23ddc35c5a3994a6600a2bb
B.Balamanigandan <bbalamanigandan@increvcorp.com>
2018-09-27 17:53:04 +0530

    Initial commit of sorting logic, which contains Bubble sort.

---
+++ datastructure/sorting/bubbleSort.js
@@ -0,0 +1,22 @@
+const bubbleSort = (list) => {
+    const n = list.length;
+    let counter = 0;
+    for (let i = 0; i < (n - 1); i++) {
+        for (let j = 0; j < (n - i - 1); j++) {
+            ++counter;
+            if(list[j] > list[j + 1]) {
+                const temp = list[j];
+                list[j] = list[j + 1];
+                list[j + 1] = temp;
+            }
+        }
+    }
+    console.log(counter);
+    return list;
+}
+
+const collection = [3,1,5,2,6];
+const result = bubbleSort(collection);
+
+console.log("Before Sorting: ", collection.toString());
+console.log("After Sorting: ", result.toString());
